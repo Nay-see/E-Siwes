95527d3842c2595b8248390042323fd4fa342d20
Nay-see <167111804+Nay-see@users.noreply.github.com>
2025-02-27 15:39:38 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+require("dotenv").config();
+const PORT = process.env.PORT || 8080;
+const DBURI = process.env.DBURI;
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const { default: mongoose } = require("mongoose");
+
+const app = express();
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+app.use(bodyParser.json());
+app.use(require("./requirements"));
+
+app.get("/", (req, res) => {
+  res.json({
+    server: true,
+    database: false,
+  });
+});
+
+mongoose.connect(DBURI).then((res) => {
+  console.log("Connected: ", true);
+  app.listen(PORT, () => console.log(`Server started on ${PORT}`));
+});
---
+++ requirements.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+
+app.use(require("./Routes/Auth/Admin/index"));
+app.use(require("./Routes/Auth/Student/index"));
+app.use(require("./Routes/Auth/Supervisor/index"));
+app.use(require("./Routes/Procedures/Admin/index"));
+app.use(require("./Routes/Procedures/Student/index"));
+require("./Modules/WebTokenAuth.js");
+
+module.exports = app;
